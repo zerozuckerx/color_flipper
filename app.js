11acd31814d7bfb8bf5d29e78640a240fd5b26a3
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-03-18 13:27:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Color Flipper</title>
+  <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+
+
+
+</body>
+</html>
