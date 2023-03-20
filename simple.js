6de0d9cf538edd893be6b38f79e1103d498a0ef2
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-03-20 11:36:08 +0100

    js for simple flipper

--- simple.js
+++ simple.js
@@ -1,1 +1,12 @@
-const colors = ["green", "yellow", "rgba(133,122,200)", "#f15025"];
+// *** VARIABLES ***
+const colors = ["Green", "Yellow", "Rgba(133,122,200)", "#F15025"];
+
+// *** BUTTON ASSIGMENTS ***
+const colorButton = document.getElementById("btn");
+
+// *** FUNCTIONS ***
+colorButton.addEventListener("click", () => {
+  const randomNumber = Math.floor(Math.random() * colors.length)
+  document.body.style.backgroundColor = colors[randomNumber];
+  document.querySelector(".color-name").textContent = colors[randomNumber];
+});
