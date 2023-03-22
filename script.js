0eb3c76d31d4eb46adc9e691874e7e7839c5eeff
Daniel Moschnitzka <d.moschnitzka@gmail.com>
2023-03-22 16:23:28 +0100

    minor

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // *** VARIABLES ***
 const colorsSimple = ["Green", "Yellow", "Rgba(133,122,200)", "#F15025"];
 const colorsHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
 let hex = false;
 
 // *** BUTTON ASSIGMENTS ***
 const colorButton = document.querySelector(".click-me");
 const switchSimpleButton = document.querySelector(".switchSimple");
 const switchHexButton = document.querySelector(".switchHex");
-// const switchButtons = document.querySelectorAll(".switch");
 
 // *** FUNCTIONS ***
 switchSimpleButton.addEventListener("click", () => {
@@ -22,19 +20,6 @@ switchHexButton.addEventListener("click", () => {
   switchHexButton.classList.add("active");
   switchSimpleButton.classList.remove("active");
 });
-
-// switchButtons.forEach(switchButton => {
-//   switchButton.addEventListener("click", () => {
-//     if(switchButton.textContent === "Hex") {
-//       hex = false;
-//       switchButton.classList.add("active");
-//       switchButton.classList.remove("active");
-//     } else {
-//       hex = true;
-//       switchButton.classList.add("active");
-//       switchButton.classList.remove("active");
-//     }
-// })});
 
 colorButton.addEventListener("click", () => {
   if(!hex) {
@@ -55,7 +40,7 @@ function simpleFlipper() {
 function hexFlipper() {
   const hexColor = ["#"];
   for(let i = 1; i <= 6; i++) {
-    let randomHexLetter = Math.floor(Math.random() * 16);
+    let randomHexLetter = Math.floor(Math.random() * colorsHex.length);
     hexColor.push(colorsHex[randomHexLetter]);
   }
   return hexColor.join("");
